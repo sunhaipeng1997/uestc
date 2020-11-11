76f4f3e09d09cc96d77cc73fd3a1ef43bb8ba6ce
Sunhaipeng <756659307@qq.com>
2020-11-11 11:21:54 +0800

    11.11修改

--- ka/src/main/java/Main2.java
+++ ka/src/main/java/Main2.java
@@ -1,7 +1,9 @@
 
+
+import com.alibaba.fastjson.JSONObject;
 import okhttp3.*;
 
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,11 +14,31 @@ import java.util.Date;
  * @create: 2020-11-03 10:35
  **/
 public class Main2 {
+    private static String getCookie() throws FileNotFoundException {
+        File file = new File("weu.txt");
+        if(file.isFile() && file.exists()){
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuffer sb = new StringBuffer();
+                String text = null;
+                while((text = bufferedReader.readLine()) != null){
+                    sb.append(text);
+                }
+                return sb.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
 
 
     public static void main(String[] args) throws IOException {
+        String cookie = getCookie();
+        System.out.println(cookie);
 
-        String cookie = "route=e3a7da32b2f8e62c5aa5e51f0d7a4589; EMAP_LANG=zh; THEME=indigo; _WEU=XulKGjzMY5AweaJAw27Jl2IJpN39OH1weHGbgz9vdnnX4h_z7fFS4oXFh__q3mjqzNR_oQ54ajIGIpOSzKaTw5*6a5ti5Ml9*y*x4SvYg7ebg5wIS5YSS_Y6H4Ptp2gEVx9lOhZUy1*VrM_UmiG7Cc..; UM_distinctid=172b72aa7d6405-03863fe9460dd-5d462912-240000-172b72aa7d7872; _ga=GA1.3.1214256026.1594967766; loginServiceserchVal=%u6BD5%u4E1A%u751F%u4E2D%u6587%u6210%u7EE9%u53CA%u5B66%u7C4D%u8BC1%u660E%u6863%u6848%u670D%u52A1; loginServiceroleId=all; loginServiceclassifyId=all; loginServiceSearchVal=; openLoginServicePageFlag=false; route=30dfce7b7500cd543e989b26cda7c8b4; iPlanetDirectoryPro=VLVzxYgA1bndQDsFxZki2T; MOD_AUTH_CAS=MOD_AUTH_ST-99562-03t35EhZbhtbY1oH1GV11604452686756-bLvu-cas; asessionid=4c4027cd-c914-45ba-9b46-45164444e8e7; amp.locale=undefined; JSESSIONID=4CSWWTyVNEEV9ZnePOSyaewQl1hk-YGun1_xdARsaqiM_-QrPALZ!-90893721; zg_did=%7B%22did%22%3A%20%22173ea87c9426c4-04609d9b0f99ea-5d462912-240000-173ea87c9434f2%22%7D; zg_=%7B%22sid%22%3A%201604545233178%2C%22updated%22%3A%201604545233189%2C%22info%22%3A%201604285948419%2C%22superProperty%22%3A%20%22%7B%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22eportal.uestc.edu.cn%22%2C%22cuid%22%3A%20%22201922080813%22%2C%22zs%22%3A%200%2C%22sc%22%3A%200%2C%22firstScreen%22%3A%201604545233178%7D";
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
@@ -39,8 +61,12 @@ public class Main2 {
                 .build();
         Response response = client.newCall(request).execute();
         final String res = response.body().string();
+        JSONObject jsonObject = JSONObject.parseObject(res);
+        String wid = jsonObject.getJSONObject("datas").getJSONObject("getMyTodayReportWid").getJSONArray("rows").getJSONObject(0).get("WID").toString();
+
+
         System.out.println(res);
-        String wid = res.substring(92, 124);
+//        String wid = res.substring(92, 124);
         System.out.println(wid);
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date(System.currentTimeMillis());
